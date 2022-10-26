fc0b16cfac91298e5f24ca0d2447a4c905bdeb30
Evan <evan.gassiot@colorado.edu>
2022-10-25 22:50:20 -0600

    added comments

--- testing.m
+++ testing.m
@@ -1,7 +1,19 @@
-X_int = imread('square.jpg');
-X_double = double(X_int);
-A = flip(eye(1024));
+%% As far as I'm aware this will only work on square images not rectangular ones
 
-X_flip = A*X_double(:,:,1)*A + A*X_double(:,:,2)*A + A*X_double(:,:,3)*A;
+X_int = imread('square.jpg'); % Read in the image
+X_double = double(X_int); % Convert to a double
+A = flip(eye(1024)); % This creates a flipped identity matrix of size 1024x1024 which is the size of the image
+
+% | 0 0 1 | %
+% | 0 1 0 | %
+% | 1 0 0 | %
+
+% This makes use of hmwk 6, we learned that left hand multiplication swaps
+% rows and right hand mult. swaps columns. If we use both at the same time
+% we can flip an image 180 degrees as seen in this example
+
+X_flip(:,:,1) = A*X_double(:,:,1)*A;
+X_flip(:,:,2) = A*X_double(:,:,2)*A;
+X_flip(:,:,3) = A*X_double(:,:,3)*A;
 
 imagesc(uint8(X_flip))
