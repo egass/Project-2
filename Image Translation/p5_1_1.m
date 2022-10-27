c5d9d8e6b1cdd6e13421ff2703f5fab93be529d0
Evan <evan.gassiot@colorado.edu>
2022-10-27 13:14:18 -0600

    problem 2

--- Image Translation/p5_1_1.m
+++ Image Translation/p5_1_1.m
@@ -1,6 +1,7 @@
 X = imread('rectangle.jpg'); %% read in image
 X_double = double(X); %% convert image to double form
 X_gray= X_double(:,:,1)/3.0 + X_double(:,:,2)/3.0 + X_double(:,:,3)/3.0; %% following 3 lines convert image to gray scale
+map = colormap('gray');
 imagesc(uint8(X_gray))
-colormap('gray')
-imwrite(X_gray, 'grayscaleRectange5_1_1.jpg') %% write new picture to directory
+
+imwrite(X_gray,map,'grayscaleRectange5_1_1.jpg') %% write new picture to directory
---
+++ Image Translation/p5_1_2.m
@@ -0,0 +1,20 @@
+X = imread('grayscaleRectange5_1_1.jpg'); %% Reading the grayscale image
+X_double = double(X); %% convert to double
+
+X_2intensity(:,:,1) = X_double(:,:,1)*2; %% changing the RBG channels to have double intensity
+X_2intensity(:,:,2) = X_double(:,:,2)*2;
+X_2intensity(:,:,3) = X_double(:,:,3)*2;
+
+
+figure(); %% Creating a figure
+hold on
+subplot(1,2,1); %% Left Subplot
+imagesc(uint8(X_double))
+title('Original Image')
+
+subplot(1,2,2); %% Right Subplot
+title('2x Image Intensity')
+imagesc(uint8(X_2intensity))
+title('2x Image Intensity')
+hold off
+
