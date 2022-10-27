cbc2c75b803f834e0cc89a0af05bc61e17324db5
Niko <nipa5963@colorado.edu>
2022-10-27 17:40:48 -0600

    balls

--- Image Translation/p5_1_1.m
+++ Image Translation/p5_1_1.m
@@ -1,6 +1,6 @@
 X = imread('rectangle.jpg'); %% read in image
 X_double = double(X); %% convert image to double form
-X_gray= X_double(:,:,1)/3.0 + X_double(:,:,2)/3.0 + X_double(:,:,3)/3.0; %% following 3 lines convert image to gray scale
-imagesc(uint8(X_gray))
+X_gray= X_double(:,:,1)/3.0 + X_double(:,:,2)/3.0 + X_double(:,:,3)/3.0; %% convert to gray scale
+imagesc(uint8(X_gray)) %% publish image 
 
 imwrite(uint8(X_gray),'grayscaleRectange5_1_1.jpg') %% write new picture to directory
