da181132d0d71e0494088fb970de3943962353f2
Evan <evan.gassiot@colorado.edu>
2022-11-15 12:09:18 -0700

    p5_1_3

--- Image Translation/p5_1_3.m
+++ Image Translation/p5_1_3.m
@@ -4,6 +4,7 @@ X_double = double(X); %% Convert values to doubles
 eighty = 80*ones(rows,columns); %% Matrix where every value is 80
 
 X_modified(:,:,1) = X_double(:,:,1) .* 0; %% Multiplying the red channel values by 0
+X_modified(:,:,2) = X_double(:,:,2);
 X_modified(:,:,3) = X_double(:,:,3) + eighty; %% Adding 80 to every value in the matrix
 
 imagesc(uint8(X_modified)) %% Printing the image
