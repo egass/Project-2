709f93548fa30b69c351fea4df582258080515ce
Evan <evan.gassiot@colorado.edu>
2022-11-22 16:42:54 -0700

    evan

--- Image Translation/p5_1_8.m
+++ Image Translation/p5_1_8.m
@@ -1,4 +1,9 @@
 A = imread('rectangle.jpg');
-B = permute(A, [2 1 3]); %% switch rows and coloums
-imagesc(B);
-imwrite(B,'transposedRectangle.jpg')
+%{
+The image matrix here is m x n x c, what permute does is this:
+...[2 1 3] says to swap m & n but leave c where it already is.
+%}
+
+B = permute(A, [2 1 3]); %
+imagesc(uint8(B));
+%%imwrite(B,'transposedRectangle.jpg')
